f764ba55e3a5610f7e3ecb956ca5054f43481a98
dhruv sakariya <dhruvsakariya2304@gmail.com>
2024-01-23 08:20:27 +0530

    🧪 Testing Deployment

--- index.js
+++ index.js
@@ -1,42 +1,61 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const express = require('express');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const app = express();
-const port = 3001;
+// const app = express();
+// const port = 3001;
 
-// Middleware to handle CORS
-app.use((req, res, next) => {
-    const allowedOrigin = req.headers.origin; // Set to the origin of the incoming request
+// // Middleware to handle CORS
+// app.use((req, res, next) => {
+//     const allowedOrigin = req.headers.origin; // Set to the origin of the incoming request
 
-    res.header('Access-Control-Allow-Origin', allowedOrigin);
-    res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-        return res.status(200).json({});
-    }
-    next();
+//     res.header('Access-Control-Allow-Origin', allowedOrigin);
+//     res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//     if (req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
+
+// // Configuration for proxying all HTTP and WebSocket requests
+// const proxy = createProxyMiddleware({
+//     target: 'https://api.jdoodle.com',
+//     changeOrigin: true,
+//     logLevel: 'debug',
+//     ws: true,
+//     onProxyRes: function (proxyRes, req, res) {
+//         const allowedOrigin = req.headers.origin;
+//         proxyRes.headers['Access-Control-Allow-Origin'] = allowedOrigin;
+//         proxyRes.headers['Access-Control-Allow-Credentials'] = 'true';
+//     }
+// });
+
+// // Apply the proxy to all incoming requests
+// app.use('/', proxy);
+
+// // Start the server
+// app.listen(port, () => {
+//     console.log(`Proxy server listening on port ${port}`);
+// });
+
+// module.exports = app;
+
+
+// index.js
+const express = require('express')
+const app = express()
+const PORT = 4000
+
+
+app.get('/home', (req, res) => {
+    res.status(200).json('Welcome, your app is working well');
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
 
-// Configuration for proxying all HTTP and WebSocket requests
-const proxy = createProxyMiddleware({
-    target: 'https://api.jdoodle.com',
-    changeOrigin: true,
-    logLevel: 'debug',
-    ws: true,
-    onProxyRes: function (proxyRes, req, res) {
-        const allowedOrigin = req.headers.origin;
-        proxyRes.headers['Access-Control-Allow-Origin'] = allowedOrigin;
-        proxyRes.headers['Access-Control-Allow-Credentials'] = 'true';
-    }
-});
-
-// Apply the proxy to all incoming requests
-app.use('/', proxy);
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Proxy server listening on port ${port}`);
-});
-
-module.exports = app;
+// Export the Express API
+module.exports = app
